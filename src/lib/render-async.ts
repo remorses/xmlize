1a1578a03678e44907f7f2135ea35fdc8470e6f8
remorses <beats.by.morse@gmail.com>
2025-02-27 00:46:01 +0100

    decrease the number of async functions, better performance for large trees

--- src/lib/render-async.ts
+++ src/lib/render-async.ts
@@ -1,13 +1,13 @@
+import { ReactElement } from 'react';
+import { isElement } from 'react-is';
 import { create } from 'xmlbuilder2';
 import type { XMLBuilder } from 'xmlbuilder2/lib/interfaces';
 import { XMLBuilderCreateOptions } from 'xmlbuilder2/lib/interfaces';
-import { isElement } from 'react-is';
+import * as builtin from '../builtin';
+import { createBuiltins } from '../builtin';
+import { isJsxXmlComponentElement, isJsxXmlTagElement } from './jsx';
 import { reactElementToJsxXmlElement } from './react';
 import { JsxXmlElement } from './types';
-import { isJsxXmlComponentElement, isJsxXmlTagElement } from './jsx';
-import * as builtin from '../builtin';
-import { ReactElement } from 'react';
-import { createBuiltins } from '../builtin';
 
 export async function renderAsync(
   element: ReactElement | JsxXmlElement,
@@ -19,23 +19,10 @@ export async function renderAsync(
     return stack[stack.length - 1];
   }
 
-  async function withElement<T>(
-    cur: XMLBuilder,
-    fn: () => Promise<T> | T,
-    stack = elementsStack,
-  ): Promise<T> {
-    stack.push(cur);
-    try {
-      return await fn();
-    } finally {
-      stack.pop();
-    }
-  }
-
   function renderElement(
     element: ReactElement | JsxXmlElement,
     stack = elementsStack,
-  ): Promise<XMLBuilder> {
+  ): Promise<XMLBuilder> | XMLBuilder {
     if (element instanceof Promise) {
       return element.then((resolved) => renderElement(resolved, stack));
     }
@@ -60,11 +47,12 @@ export async function renderAsync(
     renderAttrs(cur, element.attrs);
 
     if (element.children) {
-      await withElement(
-        cur,
-        () => renderChildren(element.children, stack),
-        stack,
-      );
+      stack.push(cur);
+      try {
+        await renderChildren(element.children, stack);
+      } finally {
+        stack.pop();
+      }
     }
     return cur;
   }
@@ -75,31 +63,35 @@ export async function renderAsync(
     }
   }
 
-  async function renderComponentElement(
+  function renderComponentElement(
     element: any,
     stack = elementsStack,
-  ): Promise<XMLBuilder> {
+  ): Promise<XMLBuilder> | XMLBuilder {
     const getter = () => getCurrentElement(stack);
 
     if (element.type === builtin.Comment) {
       const { Comment } = createBuiltins(getter);
-      return await renderChildren(Comment(element.props), stack);
+      return renderChildren(Comment(element.props), stack);
     }
     if (element.type === builtin.CData) {
       const { CData } = createBuiltins(getter);
-      return await renderChildren(CData(element.props), stack);
+      return renderChildren(CData(element.props), stack);
     }
     if (element.type === builtin.Ins) {
       const { Ins } = createBuiltins(getter);
-      return await renderChildren(Ins(element.props), stack);
+      return renderChildren(Ins(element.props), stack);
     }
-    return renderChildren(await element.type(element.props), stack);
+    let res = element.type(element.props);
+    if (res instanceof Promise) {
+      return res.then((resolved) => renderChildren(resolved, stack));
+    }
+    return renderChildren(res, stack);
   }
 
-  async function renderChildren(
+  function renderChildren(
     children: any,
     stack = elementsStack,
-  ): Promise<XMLBuilder> {
+  ): Promise<XMLBuilder> | XMLBuilder {
     const cur = getCurrentElement(stack);
 
     if (typeof children === 'string') {
@@ -107,37 +99,41 @@ export async function renderAsync(
     } else if (typeof children === 'number') {
       cur.txt(children.toString());
     } else if (Array.isArray(children)) {
-      const elements = await Promise.all(
+      return Promise.all(
         children.map((child) => {
           const childStack = [...stack];
           return renderChildren(child, childStack);
         }),
-      );
-      // reorder elements to be same as promise.all
-      cur.toArray(false).forEach((x) => {
-        // Skip removing comments, CDATA sections, and processing instructions
-        if (
-          x.node.nodeType !== 8 && // Comment
-          x.node.nodeType !== 4 && // CDATA
-          x.node.nodeType !== 7 // Ins
-        ) {
-          x.remove();
-        }
+      ).then((elements) => {
+        // reorder elements to be same as promise.all
+        cur.toArray(false).forEach((x) => {
+          // Skip removing comments, CDATA sections, and processing instructions
+          if (
+            x.node.nodeType !== 8 && // Comment
+            x.node.nodeType !== 4 && // CDATA
+            x.node.nodeType !== 7 // Ins
+          ) {
+            x.remove();
+          }
+        });
+
+        elements.forEach((element) => {
+          // Skip importing if element is the same as cur to avoid circular references
+          if (element !== cur) {
+            cur.import(element);
+          }
+        });
+
+        return cur;
       });
-      for (const element of elements) {
-        // Skip importing if element is the same as cur to avoid circular references
-        if (element !== cur) {
-          cur.import(element);
-        }
-      }
-      return cur;
     } else if (children) {
-      return await renderElement(children, stack);
+      return renderElement(children, stack);
     }
     return cur;
   }
 
   let cur = create(options ?? {});
-  await withElement(cur, () => renderElement(element), elementsStack);
+  elementsStack.push(cur);
+  await renderElement(element, elementsStack);
   return cur;
 }
